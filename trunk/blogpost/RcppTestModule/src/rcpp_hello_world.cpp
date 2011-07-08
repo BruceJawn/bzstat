763c7c9d5dd057c1a426713e4d56ef5401b03fb6
zhoubu1988@gmail.com <zhoubu1988@gmail.com@a843961c-6848-b950-5f75-aa70fdd9a738>
2011-07-08 11:35:07 +0000

    Rcpptest

---
+++ trunk/blogpost/RcppTestModule/src/rcpp_hello_world.cpp
@@ -0,0 +1,15 @@
+//rcpp_hello_world.cpp
+#include "rcpp_hello_world.h"
+
+using namespace Rcpp;
+
+double norm(double x, double y){
+return sqrt(x*x+y*y);
+}
+
+RCPP_MODULE(mod){
+ function("norm", 
+	      &norm,
+		  List::create( _["x"] = 0.0, _["y"] = 0.0),
+		  "Provides a simple vector norm");
+}
